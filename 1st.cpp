237ff62c843eff9f9af8e3cdc8cd231d2709f27c
Brijrajsinh Gohil <brijrajsinhg228@gmail.com>
2023-01-21 15:18:22 +0530

    intitial commit

---
+++ 1st.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+//fixed<<setprecision(15)<<sum
+#define PB push_back
+#define BS binary_search
+#define floop(i,a,b) for(int i=a;i<b;i++)
+#define ALL(v) v.begin(),v.end()
+#define cinAr(a) for(auto &x:a)cin>>x;
+#define F first
+#define S second
+#define mod 1000000007
+#define PI 3.141592653589793238462
+void IOS()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+void solve(){
+    
+}
+int main()
+{
+    IOS();
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t=1;
+    int x = 0;
+    x++;
+    cout<<x<<"\n";
+    return 0;
+}
